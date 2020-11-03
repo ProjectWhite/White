8883c9608dae8a4e5a86f02b7ba76c5d7d17ab66
HiwteenTeen <Easily_wayto_devoloper@users.noreply.github.com>
2020-11-03 20:21:16 +0700

    Create Milestone.dart

---
+++ flutter_appchatbot/lib/Pagechat/Milestone.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Milestone extends StatefulWidget {
+  @override
+  _MilestoneState createState() => _MilestoneState();
+}
+
+class _MilestoneState extends State<Milestone> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
