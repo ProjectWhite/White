f6d0bcbd68b30aa12cb1cadad9c754e894f1d3d0
HiwteenTeen <Easily_wayto_devoloper@users.noreply.github.com>
2020-11-03 22:57:54 +0700

    Update Milestone.dart

--- flutter_appchatbot/lib/Pagechat/Milestone.dart
+++ flutter_appchatbot/lib/Pagechat/Milestone.dart
@@ -7,6 +7,10 @@ class Milestone extends StatefulWidget {
 
 class _MilestoneState extends State<Milestone> {
   @override
+  final angry = Colors.red;
+  final PageController ctrl = PageController();
+
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.greenAccent,
@@ -15,7 +19,93 @@ class _MilestoneState extends State<Milestone> {
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
-      body: Text('Test'),
+      body: PageView(
+        scrollDirection: Axis.horizontal,
+        controller: ctrl,
+        children: [
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text('Date',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),),
+                ),
+                CircleAvatar(
+                  backgroundColor: Colors.red,
+                  radius: 100,
+                  child: Center(
+                    child: Text('angry',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text('Date',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),),
+                ),
+                CircleAvatar(
+                  backgroundColor: Colors.green,
+                  radius: 100,
+                  child: Center(
+                    child: Text('happy',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text('Date',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),),
+                ),
+                CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  radius: 100,
+                  child: Center(
+                    child: Text('sad',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
