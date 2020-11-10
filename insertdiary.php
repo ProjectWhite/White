bf399710df2e1e0e0fa2cb7bf5f442915ced6ed8
Aoystwr <59905968+nuchhbs@users.noreply.github.com>
2020-11-10 15:07:40 +0700

    ag

---
+++ insertdiary.php
@@ -0,0 +1,12 @@
+<?php
+    include 'conn.php';
+    
+    $username = $_POST['username'];
+    $message = $_POST['message'];
+    date_default_timezone_set('Asia/Bangkok');
+    $date = date('Y-m-d H:i:s');
+    
+    $connect->query("INSERT INTO milestone(username,date,diary) VALUES('".$username."','".$date."','".$message."')");
+
+
+    ?>
