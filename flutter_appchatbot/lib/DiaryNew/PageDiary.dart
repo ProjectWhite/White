b2ce513b9b32e4bcbd41d1a7cf1ea1b68ad8f679
HiwteenTeen <Easily_wayto_devoloper@users.noreply.github.com>
2020-11-26 02:28:31 +0700

    l,

--- flutter_appchatbot/lib/DiaryNew/PageDiary.dart
+++ flutter_appchatbot/lib/DiaryNew/PageDiary.dart
@@ -34,23 +34,13 @@ class Diary extends StatefulWidget {
 
 class _HomeState extends State<Diary> {
 
-  Future<List<dynamic>> _future;
-
-  String _timeString;
+  Future<List<dynamic>> _aoyler;
   @override
   void initState() {
     super.initState();
-    _future = _readdiary();
-    _timeString = "${DateTime.now().hour.toString().padLeft(2, '0')} : ${DateTime.now().minute.toString().padLeft(2, '0')}";
-    Timer.periodic(Duration(seconds:1), (Timer t)=>_getCurrentTime());
-    super.initState();
+    _aoyler = _readdiary();
   }
 
-  void _getCurrentTime()  {
-    setState(() {
-      _timeString = "${DateTime.now().hour.toString().padLeft(2, '0')} : ${DateTime.now().minute.toString().padLeft(2, '0')}";
-    });
-  }
 
   final messageInsert = TextEditingController();
   final msg = TextEditingController();
@@ -98,91 +88,85 @@ class _HomeState extends State<Diary> {
 
   @override
   Widget build(BuildContext context) {
+
     print('c');
     print(c);
     print('check');
     print(check);
     int _currentIndex = 0;
-    return FutureBuilder(
-      future: _future,
-      builder: (BuildContext context,AsyncSnapshot snap){
-        if(check==0) {
-          check=2;
-        print('loading');
-        print(check);
-        return
-          SpinKitFadingCube(
-              color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0),
-            size: 50.0,duration: Duration(seconds: 1)
-          );
-      }
-      if(check==2){
-        return Container(
-          color: Colors.white,
-          child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: Icon(
-                      Icons.menu_book_rounded,
-                      size: 50,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  Text(
-                    'Write diary now',
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ],
-              )),
-        );
-      }
-      return Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.bottomLeft,
-                end: Alignment.topRight,
-                colors: [HexColor('#FFFFFF'), HexColor('#FFFFFF')]
-            ),
-          ),
-          child: Column(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(top: 35, bottom: 10),
-                child: Text("$name $_timeString",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontFamily: 'RobotoCondensed',
-                    fontWeight: FontWeight.bold,
-                  ),),
-              ),
-              Flexible(
-                  child: ListView.builder(
-                      reverse: false,
-                      itemCount: messsages.length,
-                      itemBuilder: (context, index) =>
-                          chat(
-                            messsages[index]["message"].toString(),
-                            messsages[index]["date"].toString(),
-                            messsages[index]["emo"].toString(),
-                            messsages[index]["type"].toString(),
-                          )
-                  )),
-            ],
+
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              colors: [HexColor('#FFFFFF'), HexColor('#FFFFFF')]
           ),
         ),
-      );
-      },
+        child: Column(
+          children: <Widget>[
+            FutureBuilder(
+                future: _aoyler,
+                builder: (BuildContext context,AsyncSnapshot snap) {
+                  if (check == 0) {
+                    check = 2;
+                    print('loading');
+                    print(check);
+                    return
+                      Expanded(
+                        child: SpinKitFadingCube(
+                            color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
+                                .withOpacity(1.0),
+                            size: 50.0, duration: Duration(seconds: 1)
+                        ),
+                      );
+                  }
+                  if (check == 2) {
+                    return Expanded(
+                      child: Center(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              SizedBox(
+                                width: 50,
+                                height: 50,
+                                child: Icon(
+                                  Icons.menu_book_rounded,
+                                  size: 50,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              Text(
+                                'Write diary now',
+                                style: TextStyle(color: Colors.grey),
+                              )
+                            ],
+                          )),
+                    );
+                  }
 
+                  return Flexible(
+                      child: ListView.builder(
+                          reverse: false,
+                          itemCount: messsages.length,
+                          itemBuilder: (context, index) =>
+                              chat(
+                                messsages[index]["message"].toString(),
+                                messsages[index]["date"].toString(),
+                                messsages[index]["emo"].toString(),
+                                messsages[index]["type"].toString(),
+                              )
+                      ));
+                }
+            ),
+          ],
+        ),
+      ),
     );
 
-   }
+  }
 
 
   Widget chat(String message,String date,String emotionxx,String type) {
@@ -313,86 +297,3 @@ class _HomeState extends State<Diary> {
     );
   }
 }
-
-
-
-// Widget _buildCard(String name, String price, String imgPath, bool added,
-//     bool isFavorite, context) {
-//   return Padding(
-//       padding: EdgeInsets.only(top: 5.0, bottom: 5.0, left: 5.0, right: 5.0),
-//       child: InkWell(
-//           onTap: () {
-//           },
-//           child: Container(
-//               decoration: BoxDecoration(
-//                   borderRadius: BorderRadius.circular(15.0),
-//                   boxShadow: [
-//                     BoxShadow(
-//                         color: Colors.grey.withOpacity(0.2),
-//                         spreadRadius: 3.0,
-//                         blurRadius: 5.0)
-//                   ],
-//                   color: Colors.white),
-//               child: Column(children: [
-//                 Padding(
-//                     padding: EdgeInsets.all(5.0),
-//                     child: Row(
-//                         mainAxisAlignment: MainAxisAlignment.end,
-//                         children: [
-//                           isFavorite
-//                               ? Icon(Icons.favorite, color: Color(0xFFEF7532))
-//                               : Icon(Icons.favorite_border,
-//                               color: Color(0xFFEF7532))
-//                         ])),
-//                 Hero(
-//                     tag: imgPath,
-//                     child: Container(
-//                         height: 75.0,
-//                         width: 75.0,
-//                         decoration: BoxDecoration(
-//                             image: DecorationImage(
-//                                 image: AssetImage(imgPath),
-//                                 fit: BoxFit.contain)))),
-//                 SizedBox(height: 7.0),
-//                 Text(price,
-//                     style: TextStyle(
-//                         color: Color(0xFFCC8053),
-//                         fontFamily: 'Varela',
-//                         fontSize: 14.0)),
-//                 Text(name,
-//                     style: TextStyle(
-//                         color: Color(0xFF575E67),
-//                         fontFamily: 'Varela',
-//                         fontSize: 14.0)),
-//                 Padding(
-//                     padding: EdgeInsets.all(8.0),
-//                     child: Container(color: Color(0xFFEBEBEB), height: 1.0)),
-//                 Padding(
-//                     padding: EdgeInsets.only(left: 5.0, right: 5.0),
-//                     child: Row(
-//                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                         children: [
-//                           if (!added) ...[
-//                             Icon(Icons.shopping_basket,
-//                                 color: Color(0xFFD17E50), size: 12.0),
-//                             Text('Add to cart',
-//                                 style: TextStyle(
-//                                     fontFamily: 'Varela',
-//                                     color: Color(0xFFD17E50),
-//                                     fontSize: 12.0))
-//                           ],
-//                           if (added) ...[
-//                             Icon(Icons.remove_circle_outline,
-//                                 color: Color(0xFFD17E50), size: 12.0),
-//                             Text('3',
-//                                 style: TextStyle(
-//                                     fontFamily: 'Varela',
-//                                     color: Color(0xFFD17E50),
-//                                     fontWeight: FontWeight.bold,
-//                                     fontSize: 12.0)),
-//                             Icon(Icons.add_circle_outline,
-//                                 color: Color(0xFFD17E50), size: 12.0),
-//                           ]
-//                         ]))
-//               ]))));
-// }
